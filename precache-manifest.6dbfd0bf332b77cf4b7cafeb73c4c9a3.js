595f370f4be6f6053f591367418863677231747f
Rinku Vipin Mathews <rinkuvipinmathews@gmail.com>
2020-10-23 03:15:06 +0530

    Updates

---
+++ precache-manifest.6dbfd0bf332b77cf4b7cafeb73c4c9a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8198d7fba9f4529d5bc9fec40f5ef67f",
+    "url": "/RinkuVipin/burger-app/index.html"
+  },
+  {
+    "revision": "473dfec3cef38d17638f",
+    "url": "/RinkuVipin/burger-app/static/css/main.43fc3aec.chunk.css"
+  },
+  {
+    "revision": "3e34d584bb0c0281f1a5",
+    "url": "/RinkuVipin/burger-app/static/js/2.afb4157b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RinkuVipin/burger-app/static/js/2.afb4157b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "473dfec3cef38d17638f",
+    "url": "/RinkuVipin/burger-app/static/js/main.977b7a0d.chunk.js"
+  },
+  {
+    "revision": "564d53d86f3987755a24",
+    "url": "/RinkuVipin/burger-app/static/js/runtime-main.ea726763.js"
+  },
+  {
+    "revision": "a41a846a7619680dc470dbdeec385248",
+    "url": "/RinkuVipin/burger-app/static/media/burger-logo_edited.a41a846a.jpg"
+  }
+]);
